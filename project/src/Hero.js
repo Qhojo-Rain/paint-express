6f576fa6b1d92c2f1d32c2dbf36a36a9aa129b7b
samue-klu <samuelyung758@gmail.com>
2024-12-19 17:34:48 +0000

    cc

--- project/src/Hero.js
+++ project/src/Hero.js
@@ -5,7 +5,7 @@ export default function Hero (){
     <div className="Paint">
      <h2>Our Paints</h2>
      <div className="paint-img">
-      <img src="{`${process.env.PUBLIC_URL}/images/Kodak.jpg`}"/>
+      <img src="{`${process.env.PUBLIC_URL}/images/kodak.jpg`}"/>
      </div>
     </div>
   )
